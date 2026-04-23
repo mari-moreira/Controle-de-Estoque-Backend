dab7a5ab33a16696bc4e8bf64eff8f63e807258d
PedroSD616 <pedrosantosd1234@gmail.com>
2026-04-23 12:47:43 -0300

    Classes do CRUD do módulo fornecedor

---
+++ ModuloFornecedor/Repository/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package ModuloFornecedor.Repository;
+
+import ModuloFornecedor.Entity.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+}

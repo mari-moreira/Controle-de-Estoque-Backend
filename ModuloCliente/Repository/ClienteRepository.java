799c2784646b515d0110065fcc6acefc5c33ad22
PedroSD616 <pedrosantosd1234@gmail.com>
2026-04-30 16:28:36 -0300

    Classes do CRUD do módulo Cliente

---
+++ ModuloCliente/Repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package ModuloCliente.Repository;
+
+import ModuloCliente.Entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
